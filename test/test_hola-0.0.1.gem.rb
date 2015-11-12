e6767628387ab9d5a2cdeb77acd879f80df8e7c1
Jeremy Bolding <cyberchefjay@gmail.com>
2015-11-12 15:40:29 -0600

    Initial commit to hola-0.0.1.gem.

---
+++ test/test_hola-0.0.1.gem.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestHola0.0.1.gem < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
